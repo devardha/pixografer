0ab5869c7765414f178ef65edb6e5bdc89242129
dev-ardha <ardhaydhtm@gmail.com>
2020-11-15 22:22:17 +0700

    Setup apollo client

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
+import { ApolloProvider } from '@apollo/client'
+import { useApollo } from '../apollo/client'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    const apolloClient = useApollo(pageProps.initialApolloState)
+
+    return (
+        <>
+            <ApolloProvider client={apolloClient}>
+                <Component {...pageProps} />
+            </ApolloProvider>
+        </>
+    )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,35 @@
+import gql from 'graphql-tag'
+import { useQuery } from '@apollo/client'
+import { initializeApollo } from '../apollo/client'
+
+const ViewerQuery = gql`
+  query ViewerQuery {
+    viewer {
+      id
+      name
+      status
+    }
+  }
+`
+
 export default function Home() {
-  return (
-    <>
-      Home
-    </>
-  )
+    const { data: { viewer }} = useQuery(ViewerQuery)
+    console.log(viewer)
+    return (
+        <>
+        {viewer.name}
+        </>
+    )
 }
+
+export async function getStaticProps() {
+    const apolloClient = initializeApollo()
+  
+    await apolloClient.query({ query: ViewerQuery,})
+  
+    return {
+        props: {
+            initialApolloState: apolloClient.cache.extract(),
+        },
+    }
+  }
