cb2cc1727a47231952c7fc82a206483584ee3285
dev-ardha <ardhaydhtm@gmail.com>
2020-11-17 23:49:03 +0700

    Remove rest api

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import { useApollo } from '../apollo/client'
 import { wrapper } from '../redux/store'
 import GlobalStyle from '../styles/GlobalStyle'
 import NextNProgress from '../components/NextNProgress'
-import Cookies from 'cookies'
 
 function MyApp({ Component, pageProps, user }) {
     const apolloClient = useApollo(pageProps.initialApolloState)
@@ -30,29 +29,6 @@ MyApp.getInitialProps = async ({Component, ctx}) => {
     
     if (Component.getInitialProps) {
         pageProps = await Component.getInitialProps(ctx)
-    }
-
-    if(!process.browser){
-        const cookies = new Cookies(ctx.req, ctx.res);
-        const token = cookies.get('_usid')
-        const user = {
-            token: token
-        }
-
-        const res = await fetch('http://localhost:3000/api/auth', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json;charset=utf-8',
-            },
-            credentials: 'include',
-            body: JSON.stringify(user)
-        })
-        const data = await res.json()
-
-        return {
-            pageProps,
-            user: data
-        }
     }
 
     return {
